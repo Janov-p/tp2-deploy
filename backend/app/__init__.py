fc24c639a8d42530bdb1e2a3ac646a4167b1059d
Janov <achillepoirier01@gmail.com>
2025-05-07 15:34:31 +0200

    fix: cors corrected

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -35,7 +35,7 @@ def create_app(config_name='development'):
     # Configure CORS
     CORS(app, 
          resources={r"/*": {
-             "origins": ["http://localhost:5173", "https://your-frontend-domain.com"],
+             "origins": ["http://localhost:5173", "https://tp-deploy.web.app"],
              "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
              "allow_headers": ["Content-Type", "Authorization"],
              "supports_credentials": True,
